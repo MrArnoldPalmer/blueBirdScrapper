fc992fd862bc771d95f42cabae12494f7a9f9fac
jadex1 <greatwolf3d@gmail.com>
2016-06-03 11:08:53 -0700

    Added Notes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,6 @@
 import Ember from 'ember';
-
+// NOTE: This app will dispplay 10 tweets at a time and paginate to show more.
+// NOTE: The front screen will contain a simple search bar and when several conditionals,
+// NOTE: for when it has no information or no information can be found.
 export default Ember.Route.extend({
 });
